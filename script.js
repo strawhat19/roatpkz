e96e1584c83cfd986c866817aca574e5e514e19d
Furkan Coskun <furkdurk7@aol.com>
2024-02-13 12:31:34 -0500

    Login Drop Down Complete (same as More dropdown)

--- script.js
+++ script.js
@@ -5,15 +5,48 @@ let players = localStorage.getItem(`players`)
 const toggleDropDownButtonRotate = () => {
   let dropDownButton = document.querySelector(".dropdownButton");
 
+  
+  
   dropDownButton.classList.toggle(`flippingDropdownButton`);
+  
 };
+
+const toggleRegisterDropDownButtonRotate = () => {
+  let registerDropDownButton = document.querySelector(".registerDropDownButton");
+
+  
+  
+  registerDropDownButton.classList.toggle(`flippingDropdownButton`);
+  
+};
+
+// const toggleRegisterDropDownButtonRotate = () => {
+//   let registerDropDownButton = document.querySelector(".registerDropDownButton");
+  
+//   registerDropDownButton.classList.toggle(`flippingRegisterDropDownButton `);
+// }
+
 
 const toggleDropDownContent = () => {
   let dropDownContent = document.querySelector(".dropdown-content");
 
+  
+  
   // Toggle the display property of the dropdown content
   dropDownContent.style.display = dropDownContent.style.display === "none" ? "flex" : "none";
+  
 };
+
+const toggleRegisterDropDownContent = () => {
+  let registerDropDownContent = document.querySelector(".registerDropdown-content");
+  
+  registerDropDownContent.style.display = registerDropDownContent.style.display === "none" ? "flex" : "none";
+}
+
+const toggleRegisterDropDownNavLogic = () => {
+  toggleRegisterDropDownButtonRotate()
+  toggleRegisterDropDownContent()
+}
 
 // TOGGLING THE DROP DOWN ON AND OFF
 const toggleDropDownNavLogic = () => {
@@ -24,7 +57,7 @@ const toggleDropDownNavLogic = () => {
 
 //   // GetComputedStyle: This is JavaScript Method which will return an object containing the computed styles for an element
 //   const computedStyle = window.getComputedStyle(dropDownContent);
-//   // getPropertyValue: once you get the computedStyle, you can specify the PROPERTY OR CSS STYLE you want to look at "display" css in this case
+// getPropertyValue: once you get the computedStyle, you can specify the PROPERTY OR CSS STYLE you want to look at "display" css in this case
 //   const currentDisplay = computedStyle.getPropertyValue("display");
 
 //   // the terinary statement says that if the display = none, it will return the value before the colon AKA display = "flex", If not it will return Display = "none"
@@ -34,11 +67,15 @@ const toggleDropDownNavLogic = () => {
 //   toggleDropDownButtonRotate();
 };
 
-// defining button variable by querySelecting .dropdown
+// defining button variable by querySelecting .dropdownbutton
 let dropDownButton = document.querySelector(".dropdownButton");
+
+let registerDropDownButton = document.querySelector(".registerDropDownButton");
 
 // event listener for clicking the button which will open the drop down
 dropDownButton.addEventListener("click", toggleDropDownNavLogic);
+
+registerDropDownButton.addEventListener("click", toggleRegisterDropDownNavLogic);
 
 const setHeaderDynamically = () => {
   const header = $(`
